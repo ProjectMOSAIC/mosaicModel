2d302baf60c0035ef35d0521ced01eac23bebf7e
Danny Kaplan <kaplan@macid28146.home>
2018-02-12 00:46:35 -0600

    Fix for issue 6

--- R/interval_statistics.R
+++ R/interval_statistics.R
@@ -1,9 +1,10 @@
 #' Interval statistics for use with df_stats()
 #' 
-#' Function builders for calculating intervals. These must *always* be evaluated
-#' with the *result* being handed as a argument to `df_stats()`.
+#' Calculate coverage intervals and confidence intervals for the mean, median, sd
 #'
+#' @param x A vector of data
 #' @param level Number in 0 to 1 specifying the confidence level for the interval. (Default: 0.95)
+#' @param na.rm if `TRUE` disregard missing data
 #' 
 #' @rdname intervals
 #' @aliases ci.mean ci.median ci.sd 
@@ -13,72 +14,64 @@
 #' df_stats(hp ~ cyl, data = mtcars, c95 = cover)
 #' 
 #' @export
-coverage <- function(level = 0.95) {
-  function(x, na.rm = TRUE) {
-    bottom = (1 - level) / 2
-    top = 1 - bottom
-    res <- quantile(x, probs = c(bottom, top), 
-                    na.rm = na.rm, type = 7, names = TRUE)
-    names(res) <- paste("coverage", c("lower", "upper"), sep = "_")
-    names(res) <- c("lower", "upper") # Do we prefer a short form?
-    
-    res
-  }
+coverage <- function(x, level = 0.95, na.rm = TRUE) {
+  level <- check.level(level)
+  bottom = (1 - level) / 2
+  top = 1 - bottom
+  res <- quantile(x, probs = c(bottom, top), 
+                  na.rm = na.rm, type = 7, names = TRUE)
+  names(res) <- paste("coverage", c("lower", "upper"), sep = "_")
+  names(res) <- c("lower", "upper") # Do we prefer a short form?
+  
+  res
 }
 
 #' @export
-ci.mean <- function(level = 0.95) {
+ci.mean <- function(x, level = 0.95, na.rm = TRUE) {
   level <- check.level(level)
   
-  function(x, na.rm = TRUE) {
-    n <- length(x)
-    lowerq <- stats::qt((1 - level) / 2, df = n - 1)
-    res <- 
-      base::mean(x, na.rm = na.rm) + 
-        c(lowerq, -lowerq) *
-        stats::sd(x, na.rm = na.rm)/sqrt(length(x))
-    
-    # names(res) <- paste("ci_mean", c("lower", "upper"), sep = "_")
-    names(res) <- c("lower", "upper") # Do we prefer a short form?
-    
-    res
-  }
-}
-
-#' @export
-ci.median <- function(level = 0.95) {
-  level <- check.level(level)
+  n <- length(x)
+  lowerq <- stats::qt((1 - level) / 2, df = n - 1)
+  res <- 
+    base::mean(x, na.rm = na.rm) + 
+    c(lowerq, -lowerq) *
+    stats::sd(x, na.rm = na.rm)/sqrt(length(x))
   
-  function(x, na.rm = TRUE) {
-    x <- sort(x)
-    n <- length(x)
-    bottom <- (1 - level) / 2
-    top <- 1 - bottom
-    ci_index <- (n + qnorm(c(bottom, top)) * sqrt(n))/2 + c(0,1)
-    res <- as.numeric(x[round(ci_index)])
-    
-    #names(res) <- paste("ci_median", c("lower", "upper"), sep = "_")
-    names(res) <- c("lower", "upper") 
-    res
-  }
+  # names(res) <- paste("ci_mean", c("lower", "upper"), sep = "_")
+  names(res) <- c("lower", "upper") # Do we prefer a short form?
+  
+  res
 }
 
 #' @export
-ci.sd <- function(level = 0.95) {
+ci.median <- function(x, level = 0.9, na.rm = TRUE5) {
   level <- check.level(level)
+  x <- sort(x)
+  n <- length(x)
+  bottom <- (1 - level) / 2
+  top <- 1 - bottom
+  ci_index <- (n + qnorm(c(bottom, top)) * sqrt(n))/2 + c(0,1)
+  res <- as.numeric(x[round(ci_index)])
+  
+  #names(res) <- paste("ci_median", c("lower", "upper"), sep = "_")
+  names(res) <- c("lower", "upper") 
+  res
+}
 
-  function(x, na.rm = TRUE) {
-    n <- length(x)
-    bottom <- (1 - level) / 2
-    top <- 1 - bottom
-    res = (n - 1) * 
-      stats::sd(x, na.rm = TRUE)^2 /
-      qchisq(c(top, bottom), df = n - 1)
-    res <- sqrt(res)
-    #names(res) <- paste("ci_sd", c("lower", "upper"), sep = "_")
-    names <- c("lower", "upper") 
-    res 
-  }
+
+#' @export
+ci.sd <- function(x, level = 0.95, na.rm = TRUE) {
+  level <- check.level(level)
+  n <- length(x)
+  bottom <- (1 - level) / 2
+  top <- 1 - bottom
+  res = (n - 1) * 
+    stats::sd(x, na.rm = TRUE)^2 /
+    qchisq(c(top, bottom), df = n - 1)
+  res <- sqrt(res)
+  #names(res) <- paste("ci_sd", c("lower", "upper"), sep = "_")
+  names <- c("lower", "upper") 
+  res 
 }
 
 #' Function builder for proportions.
