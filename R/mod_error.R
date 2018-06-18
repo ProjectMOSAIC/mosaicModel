d9b8b6df04839ed73826e75e1a09e8ba7b8ea511
Randall Pruim <rpruim@gmail.com>
2018-06-18 14:10:33 -0400

    add mod_output() and use it in mod_error()

--- R/mod_error.R
+++ R/mod_error.R
@@ -1,16 +1,16 @@
 #' Mean square prediction error
 #'
 #' Compares model predictions to the actual value of the response variable.
-#' To do this, testing data must be provided with *both* the input variables and the 
-#' corresponding response variable. The measure calculated for a quantitative response 
-#' variable is the mean square prediction error (MSPE). 
-#' For categorical response variables, an analog of MSPE can be calculated (see details) 
+#' To do this, testing data must be provided with *both* the input variables and the
+#' corresponding response variable. The measure calculated for a quantitative response
+#' variable is the mean square prediction error (MSPE).
+#' For categorical response variables, an analog of MSPE can be calculated (see details)
 #' but by default, a mean log-likelihood (mean per case) is computed instead.
-#' 
+#'
 #' @param model The model whose prediction error is to be estimated.
 #' @param testdata A data frame giving both model inputs and the actual value of the response
 #' variable. If no testing data is provided, the training data will be used and a warning issued.
-#' @param error_type The measure of error you are interested in. By default, this is mean-square error for 
+#' @param error_type The measure of error you are interested in. By default, this is mean-square error for
 #' regression models and log-likelihood for classifiers. The choices are:
 #' - `"mse"` -- mean square error
 #' - `"sse"` -- sum of square errors
@@ -18,48 +18,48 @@
 #' - `"LL"`  -- log-likelihood
 #' - `"mLL"` -- mean log-likehood (per case in the testing data)
 #' - `"dev"` -- deviance. (Plus a constant, which is often zero. The constant is fixed for a given testing data set,
-#' regardless of the model. So differences between deviances of two models are correct.) 
+#' regardless of the model. So differences between deviances of two models are correct.)
 #' - `"class_error"` -- classification error rate.
-#' 
-#' @details When the response variable is categorical, the model 
-#' (called a 'classifier' in such situations) must be capable of 
-#' computing *probabilities* for each output rather than just a bare category. 
+#'
+#' @details When the response variable is categorical, the model
+#' (called a 'classifier' in such situations) must be capable of
+#' computing *probabilities* for each output rather than just a bare category.
 #' This is true for many commonly encountered classifier model architectures.
-#' 
+#'
 #' The analog of the mean squared error for classifiers is the mean of (1-p)^2, where p is the
-#' probability assigned by the model to the actual output. This is a rough approximation 
+#' probability assigned by the model to the actual output. This is a rough approximation
 #' to the log-likelihood. By default, the log-likelihood will be calculated, but for pedagogical
-#' reasons you may prefer (1-p)^2, in which case set `error_type = "mse"`. Classifiers can assign a probability 
+#' reasons you may prefer (1-p)^2, in which case set `error_type = "mse"`. Classifiers can assign a probability
 #' of zero to the actual output, in which case the log-likelihood is `-Inf`. The `"mse"` error type avoids this.
-#' 
+#'
 #' @examples
 #' mod <- lm(mpg ~ hp + wt, data = mtcars)
 #' mod_error(mod) # In-sample prediction error.
 #' \dontrun{
 #' classifier <- rpart::rpart(Species ~ ., data = iris)
 #' mod_error(classifier)
-#' mod_error(classifier, error_type = "LL") 
+#' mod_error(classifier, error_type = "LL")
 #' # More typically
 #' inds <- sample(1:nrow(iris), size = 100)
 #' Training <- iris[inds, ]
 #' Testing  <- iris[ - inds, ]
 #' classifier <- rpart::rpart(Species ~ ., data = Training)
 #' # This may well assign zero probability to events that appeared in the
-#' # Testing data 
+#' # Testing data
 #' mod_error(classifier, testdata = Testing)
 #' mod_error(classifier, testdata = Testing, error_type = "mse")
 #' }
 #' @export
-mod_error <- function(model, testdata, 
+mod_error <- function(model, testdata,
                       error_type = c("default", "mse", "sse", "mad", "LL", "mLL", "dev", "class_error")) {
   error_type = match.arg(error_type)
-  
+
   if (missing(testdata)) {
     testdata <- data_from_mod(model)
     warning("Calculating error from training data.")
   }
   # error functions
-  mse <- function(actual, model_output) { 
+  mse <- function(actual, model_output) {
     mean((actual - model_output)^2, na.rm = TRUE)
   }
   sse <- function(actual, model_output) {
@@ -101,7 +101,7 @@ mod_error <- function(model, testdata,
   }
   # get the response values from the test data
   actual <- eval(parse(text = response_var(model)), envir = testdata)
-  model_vals <- mod_eval(model, data = testdata, append = FALSE)
+  model_vals <- mod_output(model, data = testdata, append = FALSE)
   if (is.numeric(actual)) {
     if (error_type == "default") {
       #warning('Setting error_type = "mse"')
@@ -115,7 +115,7 @@ mod_error <- function(model, testdata,
                   mll = bad_regression_error,
                   dev = bad_regression_error,
                   class_error = bad_regression_error)
-    res <- fun(actual, model_vals)             
+    res <- fun(actual, model_vals)
   }
   else {
     if (error_type == "default") {
@@ -130,7 +130,7 @@ mod_error <- function(model, testdata,
                   mll = mLL,
                   dev = dev,
                   class_error = class_error)
-    res <- fun(actual, model_vals)             
+    res <- fun(actual, model_vals)
   }
   names(res) <- error_type
   res
@@ -145,6 +145,6 @@ p_of_actual <- function(probs, actual_value) {
     inds <- actual_value == possibilities[k]
     res[inds] <- probs[inds, k]
   }
-  
+
   res
 }
