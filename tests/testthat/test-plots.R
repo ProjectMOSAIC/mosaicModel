8e0eca754072dd3d7af8ee51f289bf6dd3bf067f
Randall Pruim <rpruim@gmail.com>
2018-06-18 14:08:18 -0400

    load ggplot2 package when testing plots

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -2,7 +2,7 @@ context("Plots do what we expect")
 
 
 data(SAT, package = "mosaicData")
+library(ggplot2)
 mod1 <- lm(sat ~ poly(expend, 2), data = SAT)
-mod_plot(mod1, ~ expend, bootstrap = 10) 
-
+mod_plot(mod1, ~ expend, bootstrap = 10)
 mod_plot(mod1, interval = "confidence")
